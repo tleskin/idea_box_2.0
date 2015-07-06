fe4cafc28e6e05f26a652702c8a395defa5600e4
Thomas Leskin <tleskin@gmail.com>
2015-07-06 14:25:50 -0600

    Add ideas controller and view for index

---
+++ spec/features/user_visits_home_path_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe "User visits root path of app" do
+  it "sees the main page with app" do
+      visit root_path
+      expect(page).to have_content("Idea Box 2.0")
+  end
+end
